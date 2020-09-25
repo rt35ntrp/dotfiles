0fffd0214b4dac097e5e09b0fa96993ceaac63c9
Songwon Han <songwon.han@gmail.com>
2020-09-25 20:20:10 +0900

    update jupyter workspace path

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -263,7 +263,7 @@ c.NotebookApp.ip = '*'
 #c.NotebookApp.nbserver_extensions = {}
 
 ## The directory to use for notebooks and kernels.
-c.NotebookApp.notebook_dir = '~/workspace'
+c.NotebookApp.notebook_dir = '/Users/owen//workspace'
 
 ## Whether to open in a browser after starting. The specific browser used is
 #  platform dependent and determined by the python standard library `webbrowser`
